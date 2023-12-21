b797734280a6bf62f25a4398779081292cc88fa2
Welber Henrique <welberhenrique.costa@gmail.com>
2023-12-20 23:15:32 -0300

    Adicionando Ex10 Vetores

---
+++ JavaCourse/src/javaCourseVetores/VetoresEx10.java
@@ -0,0 +1,42 @@
+package javaCourseVetores;
+
+import java.util.Scanner;
+
+public class VetoresEx10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("----- VETORES EXERCÍCIO 9 - MAIS VELHO -----");
+		
+		int n;
+	    double media;
+
+	    System.out.print("Quantos alunos serao digitados? ");
+	    n = sc.nextInt();
+
+	    String[] nomes = new String[n];
+	    double[] notas1 = new double[n];
+	    double[] notas2 = new double[n];
+
+	    for (int i=0; i<n; i++) {
+	    	System.out.printf("Digite nome, primeira e segunda nota do %do aluno:\n", i + 1);
+	        sc.nextLine();
+	        nomes[i] = sc.nextLine();
+			notas1[i] = sc.nextDouble();
+			notas2[i] = sc.nextDouble();
+	    }
+
+	    System.out.println("Alunos aprovados:");
+
+	    for (int i=0; i<n; i++) {
+	        media = (notas1[i] + notas2[i]) / 2;
+
+	        if(media >= 6.0) {
+	            System.out.printf("%s\n", nomes[i]);
+	        }
+	    }
+		
+		sc.close();
+	}
+
+}
